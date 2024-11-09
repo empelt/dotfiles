d1e46e1e211df512a215812469c7e45251601a73
empelt <tetsuyakato2001@yahoo.co.jp>
2024-11-09 19:16:17 +0900

    fish, tmuxの追加

---
+++ README.md
@@ -0,0 +1,9 @@
+# dotfiles
+
+dotfile を管理するリポジトリ
+
+# Usage
+
+```bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/empelt/dotfiles/main/install.sh)"
+```
--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
+
 DOT_DIR="$HOME/dotfiles"
 
 # clone
 git clone https://github.com/empelt/dotfiles.git ${DOT_DIR}
+cat ${DOT_DIR}/logo.txt
 
-# link
-ln -sf $DOT_DIR/.vimrc ~/.vimrc
+# tmux
+git clone https://github.com/arcticicestudio/nord-tmux.git ~/.tmux/themes/nord-tmux
+ln -sf ${DOT_DIR}/tmux/.tmux.conf ~/.tmux.conf
+
+# fish
+mkdir -p ~/.config/fish/functions
+ln -sf ${DOT_DIR}/fish/config.fish ~/.config/fish/config.fish
+ln -sf ${DOT_DIR}/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
